f657ada2d5f8ca9d434c49558f8414e41b53074c
huanruomengyun <ruomo85@gmail.com>
2020-08-06 22:26:25 +0800

    Simple update

--- customize.sh
+++ customize.sh
@@ -19,6 +19,12 @@ mkdir -p $MODPATH$APNCONFDIR
 [[ -f "$MODPATH/APN/apns-conf.xml" ]] && mv -f $MODPATH/APN/apns-conf.xml $MODPATH/apns-conf.xml
 mv -f $MODPATH/apns-conf.xml $MODPATH$APNCONFDIR
 [[ -f "$MODPATH$APNCONFDIR/apns-conf.xml" ]] && rm -rf $MODPATH/APN
+
+# Remove stuffs that don't belong to modules
+rm -rf \
+$MODPATH/system/placeholder $MODPATH/customize.sh \
+$MODPATH/*.md $MODPATH/.git* $MODPATH/LICENSE $MODPATH/Doc 2>/dev/null
+
 # Set 
   set_perm_recursive $MODPATH 0 0 0755 0644
 
