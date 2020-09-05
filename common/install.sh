e2f2f1a4483965602671fdfda91bac86b15bf5f1
QingxuMo <ruomo85@gmail.com>
2020-09-05 22:25:42 +0800

    Fix some bugs

--- common/install.sh
+++ common/install.sh
@@ -5,9 +5,9 @@ if [ $REPLACEALL = "true" ]; then
 		if [[ ! -d "$APNCONFDIR" ]]; then
 			mkdir -p "$APNCONFDIR"
 			cp $MODPATH/APN/apns-conf.xml $APNCONFDIR
-			rm -f $MODPATH/APN/apns-conf.xml
 		fi
 	done
+	rm -f $MODPATH/APN/apns-conf.xml
 fi
 
 if [ ! -z $APNCONFDIR ]; then
@@ -15,3 +15,8 @@ if [ ! -z $APNCONFDIR ]; then
 	cp $MODPATH/APN/apns-conf.xml $APNCONFDIR
 	rm -f $MODPATH/APN/apns-conf.xml
 fi
+
+# Remove stuffs that don't belong to modules
+rm -rf \
+$MODPATH/system/placeholder $MODPATH/Doc $MODPATH/APN $MODPATH/customize.sh \
+$MODPATH/README.md $MODPATH/LICENSE $MODPATH/.git* 2>/dev/null
