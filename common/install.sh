85877f0ed230c765c170869ccfe12f72ae4ac77c
RimuruW <59136309+RimuruW@users.noreply.github.com>
2021-04-30 17:15:52 +0800

    Draft

--- common/install.sh
+++ common/install.sh
@@ -1,25 +1,24 @@
 if [ $REPLACEALL = "true" ]; then
-	for i in `find /system -name "apns-conf.xml" -type f`;
-	do
+	for i in $(find /system -name "apns-conf.xml" -type f); do
 		APNCONFDIR="$MODPATH/${i%/*}"
 		if [[ ! -d "$APNCONFDIR" ]]; then
 			mkdir -p "$APNCONFDIR"
-			cp $MODPATH/APN/apns-conf.xml $APNCONFDIR
+			cp "$MODPATH"/APN/apns-conf.xml "$APNCONFDIR"
 		fi
 	done
-	rm -f $MODPATH/APN/apns-conf.xml
-elif [ -n $APNCONFDIR ]; then
-	mkdir -p $APNCONFDIR
-	cp $MODPATH/APN/apns-conf.xml $APNCONFDIR
-	rm -f $MODPATH/APN/apns-conf.xml
+	rm -f "$MODPATH"/APN/apns-conf.xml
+elif [ -n "$APNCONFDIR" ]; then
+	mkdir -p "$APNCONFDIR"
+	cp "$MODPATH"/APN/apns-conf.xml "$APNCONFDIR"
+	rm -f "$MODPATH"/APN/apns-conf.xml
 fi
 
-if [ $UNINSTALLCARRIERSETTINGS = "true" ]; then
-  mkdir -p $MODPATH/system/product/priv-app/CarrierSettings
-  touch $MODPATH/system/product/priv-app/CarrierSettings/.replace
+if [ "$UNINSTALLCARRIERSETTINGS" = "true" ]; then
+	mkdir -p "$MODPATH"/system/product/priv-app/CarrierSettings
+	touch "$MODPATH"/system/product/priv-app/CarrierSettings/.replace
 fi
 
 # Remove stuffs that don't belong to modules
 rm -rf \
-$MODPATH/system/placeholder $MODPATH/Doc $MODPATH/APN $MODPATH/customize.sh \
-$MODPATH/README.md $MODPATH/LICENSE $MODPATH/.git* 2>/dev/null
+	"$MODPATH"/system/placeholder "$MODPATH"/Doc "$MODPATH"/APN "$MODPATH"/customize.sh \
+	"$MODPATH"/README.md "$MODPATH"/LICENSE "$MODPATH"/.git* 2>/dev/null
